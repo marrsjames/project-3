b7dfc9cd53631d71864270d6d72cfb9b9512d417
James Marrs <jamesmarrs@jmpersoallaptop.home>
2021-10-02 16:04:14 +0100

    doctors controller and CRUD function working

--- config/router.js
+++ config/router.js
@@ -3,6 +3,15 @@ import doctorsController from '../controllers/doctorsController.js'
 
 const router = express.Router()
 
-router.route('/doctors').get(doctorsController.getAllDoctors)
+router
+  .route('/doctors')
+  .get(doctorsController.getAllDoctors)
+  .post(doctorsController.createDoctor)
+
+router
+  .route('/doctors/:id')
+  .get(doctorsController.getDoctor)
+  .put(doctorsController.updateDoctor)
+  .delete(doctorsController.deleteDoctor)
 
 export default router

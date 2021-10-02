69acbbdcd15609128c5a0c226bc027f914cb2874
AKerrins <akerrins@hotmail.com>
2021-10-02 17:21:58 +0100

    2nd commit. User model Usercontroller

--- config/router.js
+++ config/router.js
@@ -2,22 +2,27 @@ import express from "express";
 import doctorsController from "../controllers/doctorsController.js";
 import servicesController from "../controllers/servicesController.js";
 import patientsController from "../controllers/patientsController.js";
+import userController from "../controllers/userController.js";
 
 const router = express.Router();
 
 router
-  .route('/doctors')
+  .route("/doctors")
   .get(doctorsController.getAllDoctors)
-  .post(doctorsController.createDoctor)
+  .post(doctorsController.createDoctor);
 
 router
-  .route('/doctors/:id')
+  .route("/doctors/:id")
   .get(doctorsController.getDoctor)
   .put(doctorsController.updateDoctor)
-  .delete(doctorsController.deleteDoctor)
+  .delete(doctorsController.deleteDoctor);
 
 router.route("/patients").get(patientsController.getAllPatients);
 
 router.route("/services").get(servicesController.getAllServices);
+
+router.route("/admin/users").get(userController.getAllUsers);
+router.route("/admin/promote").get(userController.promoteUser);
+router.route("/admin/demote").get(userController.demoteUser);
 
 export default router;

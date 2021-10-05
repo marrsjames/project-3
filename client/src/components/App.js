104838abbbf8e4d71fbcadae0ca39dc5da87eeb4
antony <dearalong@tutamail.com>
2021-10-05 19:22:37 +0100

    adding client folder for frontend

---
+++ client/src/components/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = () => <h1>Hello Anne and James</h1>;
+
+export default App;
--- config/router.js
+++ config/router.js
@@ -1,21 +1,21 @@
-import express from 'express'
-import doctorsController from '../controllers/doctorsController.js'
-import servicesController from '../controllers/servicesController.js'
-import patientsController from '../controllers/patientsController.js'
-import userController from '../controllers/userController.js'
-import appointmentsController from '../controllers/appointmentsController.js'
+import express from "express";
+import doctorsController from "../controllers/doctorsController.js";
+import servicesController from "../controllers/servicesController.js";
+import patientsController from "../controllers/patientsController.js";
+import userController from "../controllers/userController.js";
+import appointmentsController from "../controllers/appointmentsController.js";
 
 import secureRoute from "../middleware/secureRoute.js";
 
 const router = express.Router();
 
 router
-  .route('/doctors')
+  .route("/doctors")
   .get(doctorsController.getAllDoctors)
   .post(secureRoute, doctorsController.createDoctor);
 
 router
-  .route('/doctors/:id')
+  .route("/doctors/:id")
   .get(doctorsController.getDoctor)
   .put(secureRoute, doctorsController.updateDoctor)
   .delete(secureRoute, doctorsController.deleteDoctor);
@@ -47,12 +47,12 @@ router.route("/register").post(userController.registerUser);
 router.route("/login").post(userController.loginUser);
 
 router
-  .route('/appointments')
+  .route("/appointments")
   .get(appointmentsController.getAllAppointments)
-  .post(appointmentsController.createAppointment)
+  .post(appointmentsController.createAppointment);
 
-router.route('/admin/users').get(userController.getAllUsers)
-router.route('/admin/promote').get(userController.promoteUser)
-router.route('/admin/demote').get(userController.demoteUser)
+// router.route('/admin/users').get(userController.getAllUsers)
+// router.route('/admin/promote').get(userController.promoteUser)
+// router.route('/admin/demote').get(userController.demoteUser)
 
-export default router
+export default router;

296868fe60e0cde0d3fc04e44e3a714c837003e8
marrsjames <jamesmarrs93@gmail.com>
2021-10-08 23:32:04 +0100

    select date & time function working on create appt. added basic home and navbar. put doctor stuff in a folder

---
+++ client/src/components/common/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <Link to='/' className='navbar-item'>
+        Home
+      </Link>
+      <Link to='/doctors' className='navbar-item'>
+        View doctors
+      </Link>
+      <Link to='/appointments' className='navbar-item'>
+        Appointments
+      </Link>
+      <Link to='/newappointment' className='navbar-item'>
+        New Appointment
+      </Link>
+    </div>
+  )
+}
+
+export default NavBar
--- config/router.js
+++ config/router.js
@@ -12,45 +12,43 @@ const router = express.Router()
 router
   .route('/doctors')
   .get(doctorsController.getAllDoctors)
-  .post(doctorsController.createDoctor);
+  .post(doctorsController.createDoctor)
 
 router
   .route('/doctors/:id')
   .get(doctorsController.getDoctor)
   .put(doctorsController.updateDoctor)
-  .delete(doctorsController.deleteDoctor);
+  .delete(doctorsController.deleteDoctor)
 
 router
-  .route("/patients")
+  .route('/patients')
   .get(patientsController.getAllPatients) // this needs to be secure, but also should be specific to user. Patient shouldn't be able to see other patients.
-  .post(patientsController.createPatient);
-  .put(secureRoute, doctorsController.updateDoctor)
-  .delete(secureRoute, doctorsController.deleteDoctor)
+  .post(patientsController.createPatient)
+//.put(secureRoute, doctorsController.updateDoctor)
+//.delete(secureRoute, doctorsController.deleteDoctor)
 
 router
   .route('/patients')
   // .get(secureRoute, patientsController.getAllPatients) // this needs to be secure, but also should be specific to user. Patient shouldn't be able to see other patients.
   .get(patientsController.getAllPatients) //unsecure temporarily wile working
-  .post(secureRoute, patientsController.createPatient)
-
+//.post(secureRoute, patientsController.createPatient)
 
 router
   .route('/patients/:id')
   .get(patientsController.getPatient)
   .put(patientsController.updatePatient)
-  .delete(patientsController.deletePatient);
+  .delete(patientsController.deletePatient)
 
 router
   .route('/services')
   .get(servicesController.getAllServices)
-  .post(servicesController.createService);
-
+  .post(servicesController.createService)
 
 router
   .route('/services/:id')
   .get(servicesController.getService)
   .put(servicesController.updateService)
-  .delete(servicesController.deleteService);
+  .delete(servicesController.deleteService)
 
 router.route('/register').post(userController.registerUser)
 

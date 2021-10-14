c4408d7b9cd796892549003e66acc92a2e2ac363
AKerrins <akerrins@hotmail.com>
2021-10-14 23:30:57 +0100

    will try to tidy up home page tomoorrow🤞🏻

--- client/src/components/common/Home.js
+++ client/src/components/common/Home.js
@@ -1,11 +1,25 @@
 import React from "react";
+import homepicSrc from "../../images/Homepage-Pic1.jpg";
+import nhsInIT from "../../images/NHS-In-It-Together.jpg";
+import practiceNurse from "../../images/practice-nurse.jpg";
 
 const Home = () => {
   return (
-    <p>
-      This is the homepage. We will have a picture of doctors smiling and
-      laughing whilst jabbing patients.
-    </p>
+    <section className="hero is-fullheight-with-navbar is-success">
+      <div className="hero-body">
+        <div className="title is-1 has-text-centered has-text-black">
+          <h1>GA SURGERY</h1>
+          <p>
+            This is the homepage. We will have a picture of doctors smiling and
+            laughing whilst jabbing patients.
+          </p>
+
+          <img src={homepicSrc} alt="" height="500" width="500" />
+          <img src={nhsInIT} alt="" height="500" width="500" />
+          <img src={practiceNurse} alt="" height="500" width="500" />
+        </div>
+      </div>
+    </section>
   );
 };
 

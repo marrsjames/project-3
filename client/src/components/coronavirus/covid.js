d8a9163c84a8bee889e4535c642dec62d9d2c768
AKerrins <akerrins@hotmail.com>
2021-10-16 12:45:16 +0100

    the final push.

--- client/src/components/coronavirus/covid.js
+++ client/src/components/coronavirus/covid.js
@@ -1,15 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import coronavirus from "../../images/coronavirus.jpg";
 
 const Covid = () => {
   return (
-    <div>
-      <h1>Coronavirus Information</h1>
-      <h2>Travel guidance - link to an API?</h2>
-      <h2>Sections with information</h2>
-      <h2>
-        Hyperlink to our services for coranvirus vaccinations/testing/blah
-      </h2>
-      <h2>Statistics - get from an API?</h2>
+    <div className="column is-one-quarter-desktop is-one-third-tablet is-half-mobile">
+      <Link to={`/coronavirus`}>
+        <div className="card">
+          <div className="card-header" />
+          <h4 className="card-header-title">Hello SEI 21</h4>
+        </div>
+        <div className="card-image">
+          <figure className="image is-1by1">
+            <img
+              src={coronavirus}
+              alt={coronavirus}
+              loading="lazy"
+              width="225"
+              height="225"
+            />
+          </figure>
+        </div>
+
+        <div className="card-content">
+          <h5>{} data to go here</h5>
+        </div>
+      </Link>
     </div>
   );
 };
